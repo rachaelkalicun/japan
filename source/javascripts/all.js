c97075aa04279b5ae154ebfe968cef3b1f01b042
Rachael <rachael@kalicun.com>
2013-03-01 15:58:31 -0700

    hisrc jquery plugin

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
 //= require_tree .
+
+$.hisrc.speedTest();
+
+$(document).ready(function(){
+
+  $(".hisrc img").hisrc({
+    useTransparentGif: true,
+    transparentGifSrc: '/images/spacer.gif' // use for IE
+  });
+
+});
