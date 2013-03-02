9aab947674e7208c7adc9b3adfc2c886adc674c8
Rachael <rachael@kalicun.com>
2013-03-01 18:11:56 -0700

    initial navigation using flexnav plugin

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,7 +3,7 @@
 $.hisrc.speedTest();
 
 $(document).ready(function(){
-
+  $(".flexnav").flexNav();
   $(".hisrc img").hisrc({
     useTransparentGif: true,
     transparentGifSrc: '/images/spacer.gif' // use for IE
