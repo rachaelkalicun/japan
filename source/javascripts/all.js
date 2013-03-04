de0afbcd8d5a068d063c903d56df1ac99a682317
Rachael <rachael@kalicun.com>
2013-03-04 14:36:06 -0700

    removed flexnav plugin

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,7 +3,6 @@
 $.hisrc.speedTest();
 
 $(document).ready(function(){
-  $(".flexnav").flexNav();
   $(".hisrc img").hisrc({
     useTransparentGif: true,
     transparentGifSrc: '/images/spacer.gif' // use for IE
